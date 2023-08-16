0ebf0b4be49ee9a8eb1a1d391e8ab92ba6922029
kvluz <kvluz@f3m.pt>
2023-08-16 23:51:50 +0100

    share button and intent

--- app/src/main/java/com/example/chucknorrisjokes/View/MainActivity.kt
+++ app/src/main/java/com/example/chucknorrisjokes/View/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.chucknorrisjokes.View
 
 import JokeViewModel
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -19,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this).get(JokeViewModel::class.java)
         val textViewJoke: TextView = findViewById(R.id.jokeLabel)
         val buttonNewJoke: FloatingActionButton = findViewById(R.id.button)
+        val buttonShare: Button = findViewById(R.id.buttonShare)
 
         viewModel.jokeLiveData.observe(this, Observer { joke ->
             textViewJoke.text = joke.value
@@ -27,8 +30,22 @@ class MainActivity : AppCompatActivity() {
         viewModel.fetchRandomJoke()
 
 
+        // Button New Joke onclick
         buttonNewJoke.setOnClickListener {
             viewModel.fetchRandomJoke()
         }
+
+        // Button Share onclick
+        buttonShare.setOnClickListener {
+            val jokeText = textViewJoke.text.toString()
+            shareJoke(jokeText)
+        }
+    }
+
+    private fun shareJoke(jokeText: String) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, jokeText)
+        startActivity(Intent.createChooser(intent, "Share Joke"))
     }
 }
